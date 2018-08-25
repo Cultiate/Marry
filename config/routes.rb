ee602e444b25b98d00513031adb43f50b0f1db8d
Yusuke Tachibana <yusuke3627@gmail.com>
2018-08-25 21:29:51 +0900

    sessions edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root "welcome#top"
   get "about" => "welcome#about"
 
   resources :users
-  get  '/signup' => 'users#new'
-  post  '/signup' => 'users#create'
+  get  "/signup" => "users#new"
+  post  "/signup" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 
   get "projects/new" => "projects#new"
   get "projects/edit" => "projects#edit"
