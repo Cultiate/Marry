68ab4f4ad9429a4f45b0121d65eb62fe2c3a18da
Yusuke Tachibana <yusuke3627@gmail.com>
2018-08-25 15:20:52 +0900

    test delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   get "/" => "welcome#top"
   get "about" => "welcome#about"
 
+  resources :users
+
   get "projects/new" => "projects#new"
   get "projects/edit" => "projects#edit"
   get "projects/index" => "projects#index"
   get "projects/show" => "projects#show"
 
-  get "users/new" => "users#new"
-  get "users/edit" => "users#edit"
-  get "users/show" => "users#show"
 end
