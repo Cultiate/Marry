102c8dee9d24748f5210e20d21d783ba52c19987
Sakiya <s.s.senpuki@gmail.com>
2018-08-26 12:18:42 +0900

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,29 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//= hs.go-to.js
+//= hs.show-animation.js
+//= hs.slick-carousel.js
+//= hs.fancybox.js
+//= hs.validation.js
+//= hs.malihu-scrollbar.js
+//= hs.focus-state.js
+//= hs.unfold.js
+//= hs.header.js
+//= hs.core.js
+//= gmaps.min.js
+//= dzsparallaxer.js
+//= slick.js
+//= typed.min.js
+//= jquery.fancybox.min.js
+//= jquery.validate.min.js
+//= jquery.mCustomScrollbar.concat.min.js
+//= hs.megamenu.js
+//= bootstrap.min.js
+//= popper.min.js
+//= jquery-migrate.min.js
+//= jquery.min.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
