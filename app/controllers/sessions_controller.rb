800ae979a890b5122affb49bd716dc1f9c01b171
Yusuke Tachibana <yusuke3627@gmail.com>
2018-08-27 19:42:33 +0900

    basic session functions created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      flash[:success] = "ログインしました。"
+      redirect_to user_url(@user)
+    else
+      flash.now[:danger] = "メールアドレス、またはパスワードが違います。"
+      render "new"
+    end
   end
 
   def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
   end
 end
