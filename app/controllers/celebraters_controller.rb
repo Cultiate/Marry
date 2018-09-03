5b7dd900ffcc600acb71fb8aed1c2959c548355f
Yusuke Tachibana <yusuke3627@gmail.com>
2018-09-03 23:52:47 +0900

    follow sys make

--- app/controllers/celebraters_controller.rb
+++ app/controllers/celebraters_controller.rb
@@ -1,10 +1,9 @@
 class CelebratersController < ApplicationController
 
   def follow_project
-    byebug
-    @celebrater = Celebrater.new(user_id: current_user.id, project_id: params[:project_id], return_id: params[:return_id])
+    @celebrater = Celebrater.new(user_id: current_user.id, project_id: params[:project_id], return_id: params[:return_id], return_price: params[:return_price])
+    @project = Project.find_by(id: params[:project_id])
     if @celebrater.save
-      @project = Project.find_by(id: params[:project_id])
       redirect_to project_path(@project)
     else
       redirect_to root_url
