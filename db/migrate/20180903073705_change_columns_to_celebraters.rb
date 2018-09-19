ef93434d82f22dfc40809c9d2a7d2e5971f8ad25
Yusuke Tachibana <yusuke3627@gmail.com>
2018-09-19 15:25:46 +0900

    heroku

--- db/migrate/20180903073705_change_columns_to_celebraters.rb
+++ db/migrate/20180903073705_change_columns_to_celebraters.rb
@@ -1,8 +1,8 @@
 class ChangeColumnsToCelebraters < ActiveRecord::Migration[5.1]
   def change
-    change_column :celebraters, :user_id, :integer
-    change_column :celebraters, :project_id, :integer
-    change_column :celebraters, :return_id, :integer
-    change_column :celebraters, :return_price, :integer
+    change_column :celebraters, :user_id, 'integer USING CAST(column_name AS integer)'
+    change_column :celebraters, :project_id, 'integer USING CAST(column_name AS integer)'
+    change_column :celebraters, :return_id, 'integer USING CAST(column_name AS integer)'
+    change_column :celebraters, :return_price, 'integer USING CAST(column_name AS integer)'
   end
 end
