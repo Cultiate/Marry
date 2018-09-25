73316cfe9e25b8ea9cc3c36e89d31d4cc0731551
Sakiya <s.s.senpuki@gmail.com>
2018-09-25 21:36:01 +0900

    top-fin

--- db/migrate/20180903073705_change_columns_to_celebraters.rb
+++ db/migrate/20180903073705_change_columns_to_celebraters.rb
@@ -1,8 +1,8 @@
 class ChangeColumnsToCelebraters < ActiveRecord::Migration[5.1]
   def change
-    # change_column :celebraters, :user_id, 'integer USING CAST(user_id AS integer)'
-    # change_column :celebraters, :project_id, 'integer USING CAST(project_id AS integer)'
-    # change_column :celebraters, :return_id, 'integer USING CAST(return_id AS integer)'
-    # change_column :celebraters, :return_price, 'integer USING CAST(return_price AS integer)'
+     change_column :celebraters, :user_id, 'integer USING CAST(user_id AS integer)'
+     change_column :celebraters, :project_id, 'integer USING CAST(project_id AS integer)'
+     change_column :celebraters, :return_id, 'integer USING CAST(return_id AS integer)'
+     change_column :celebraters, :return_price, 'integer USING CAST(return_price AS integer)'
   end
 end
